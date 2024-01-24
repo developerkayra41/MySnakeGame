9011b7063b35b6e371826a0c24e229dcacd8f96e
kayra <kayra@DESKTOP-ANF5MEQ>
2024-01-24 13:13:10 +0300

    Add project files.

---
+++ MySnakeGame/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace MySnakeGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(10, 15);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1880, 1000);
+            this.panel1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("8514oem", 64.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(255)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(662, 431);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(600, 120);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "KAYBETTİN !";
+            this.label1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1902, 1033);
+            this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "1880; 1000";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MySnakeGame/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MySnakeGame
+{
+    public partial class Form1 : Form
+    {
+        Panel bait = new Panel();
+        Random rnd = new Random();
+
+        int locX;
+        int locY;
+
+        int yemX;
+        int yemY;
+
+
+        Panel snake = new Panel();
+        List<Panel> snakes = new List<Panel>();
+
+
+        string yon = "ust";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GoFullscreen(true);
+            panel1.Paint += Panel1_Paint;
+            AddBait();
+            AddSnake();
+
+        }
+
+        #region Form Ayarları
+        private void Panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Color borderColor = Color.Lime;
+            int borderWidth = 5;
+
+            Panel panel = (Panel)sender;
+
+            using (Pen borderPen = new Pen(borderColor, borderWidth))
+            {
+                e.Graphics.DrawRectangle(borderPen, 0, 0, panel.Width - 1, panel.Height - 1);
+
+            }
+        }
+        private void GoFullscreen(bool fullscreen)
+        {
+            if (fullscreen)
+            {
+                this.WindowState = FormWindowState.Normal;
+                this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+                this.Bounds = Screen.PrimaryScreen.Bounds;
+            }
+            else
+            {
+                this.WindowState = FormWindowState.Maximized;
+                this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            }
+        }
+        #endregion
+
+        #region AddBait
+        private void AddBait()
+        {
+
+
+            yemX = rnd.Next(1880);
+            yemY = rnd.Next(1000);
+
+            yemX -= yemX % 40;
+            yemY -= yemY % 40;
+
+            //bait = new Panel();
+            bait.BackColor = Color.Red;
+            bait.Size = new Size(40, 40);
+            bait.Location = new Point(yemX, yemY);
+            panel1.Controls.Add(bait);
+
+        }
+        #endregion
+
+        #region AddSnake
+        private void AddSnake()
+        {
+
+            snake.BackColor = Color.Lime;
+            snake.Size = new Size(40, 40);
+            snake.Location = new Point(400, 400);
+            panel1.Controls.Add(snake);
+            snakes.Add(snake);
+        }
+        #endregion
+
+        #region Keys
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.W) yon = "ust";
+
+            if (e.KeyCode == Keys.S) yon = "alt";
+
+            if (e.KeyCode == Keys.D) yon = "sag";
+
+            if (e.KeyCode == Keys.A) yon = "sol";
+
+        }
+        #endregion
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            locX = snakes[0].Location.X;
+            locY = snakes[0].Location.Y;
+
+            DidSnakeEat();
+            FollowHeadToQueue();
+            HaveCollision();
+
+            if (yon == "ust")
+            {
+        
+
+
+                if (locY > 0)
+                {
+                    locY -= 40;
+                }
+                else
+                {
+                    locY = 960;
+                }
+            }
+
+
+            if (yon == "alt")
+            {
+           
+
+                if (locY < 960)
+                {
+                    locY += 40;
+                }
+                else
+                {
+                    locY = 0;
+                }
+            }
+
+            if (yon == "sag")
+            {
+      
+
+                if (locX < 1880)
+                {
+                    locX += 40;
+                }
+                else
+                {
+                    locX = 0;
+                }
+            }
+
+            if (yon == "sol")
+            {
+ 
+                if (locX > 0)
+                {
+                    locX -= 40;
+                }
+                else
+                {
+                    locX = 1880;
+                }
+            }
+            snakes[0].Location = new Point(locX, locY);
+        }
+
+        private void DidSnakeEat()
+        {
+            if (snakes[0].Location == bait.Location)
+            {
+                panel1.Controls.Remove(bait);
+                AddBait();
+                AddQueue();
+            }
+        }
+
+        private void AddQueue()
+        {
+            Panel queue = new Panel();
+            queue.BackColor = Color.Lime;
+            queue.Size = new Size(40, 40);
+            panel1.Controls.Add(queue);
+            snakes.Add(queue);
+        }
+
+        private void FollowHeadToQueue()
+        {
+            for (int i = snakes.Count - 1; i > 0; i--)
+            {
+                snakes[i].Location = snakes[i - 1].Location;
+            }
+        }
+        private void HaveCollision()
+        {
+            for (int i = 2; i < snakes.Count; i++)
+            {
+                if (snakes[0].Location == snakes[i].Location) { timer1.Stop(); label1.Visible = true; }
+            }
+        }
+    }
+}
